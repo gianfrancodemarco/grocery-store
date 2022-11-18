ac7214f9a8b15a843a411e5b00c6fe6bbf77ea50
Gianfranco Demarco <gianfranco@hlpy.co>
2022-11-18 21:13:32 +0100

    update ci

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -26,13 +26,6 @@ class Settings(BaseSettings):
         raise ValueError(v)
 
     PROJECT_NAME: str
-    SENTRY_DSN: Optional[HttpUrl] = None
-
-    @validator("SENTRY_DSN", pre=True)
-    def sentry_dsn_can_be_blank(cls, v: str) -> Optional[str]:
-        if len(v) == 0:
-            return None
-        return v
 
     ORACLE_SERVER: str
     ORACLE_USER: str
